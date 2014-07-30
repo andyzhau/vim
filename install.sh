856819befea000028d8627736e9f5e301bdf5106
andy <andy@nodeswork.com>
2014-07-30 00:56:11 -0700

    Change install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,40 @@
 
 # backup existing files
 
-mkdir ~/.backup
+if [ ! -d "$HOME/.backup" ]; then
+  mkdir ~/.backup
 
-mv ~/.bash_profile ~/.backup
-mv ~/.bash_rc ~/.backup
-mv ~/.git-prompt.sh ~/.backup
-mv ~/.shell_prompt.sh ~/.backup
-mv ~/.vim ~/.backup
-mv ~/.vimrc ~/.backup
+  mv ~/.bash_profile ~/.backup
+  mv ~/.bashrc ~/.backup
+  mv ~/.git-prompt.sh ~/.backup
+  mv ~/.shell_prompt.sh ~/.backup
+  mv ~/.vim ~/.backup
+  mv ~/.vimrc ~/.backup
+else
+  rm -rf ~/.vim
+fi
 
 # copy setting files
 
 cp .bash_profile ~/
-cp .bash_rc ~/
+cp .bashrc ~/
 cp .git-prompt.sh ~/
 cp .shell_prompt.sh ~/
 cp -r .vim ~/
 cp .vimrc ~/
+
+if [ -d "$HOME/.vim" ]; then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+
+  rm /tmp/checkstyle-5.7-bin.zip
+
+  wget -P /tmp/ \
+    http://downloads.sourceforge.net/project/checkstyle/checkstyle/5.7/checkstyle-5.7-bin.zip
+
+  unzip -d /tmp/ /tmp/checkstyle-5.7-bin.zip
+
+  mkdir -p ~/.vim/libs
+
+  cp /tmp/checkstyle-5.7/checkstyle-5.7-all.jar ~/.vim/libs
+fi
